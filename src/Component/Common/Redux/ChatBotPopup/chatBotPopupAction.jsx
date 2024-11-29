99850ff1453d59aa313a330c1417d4a574d806f2
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2024-11-29 17:42:19 +0530

    toster

--- src/Component/Common/Redux/ChatBotPopup/chatBotPopupAction.jsx
+++ src/Component/Common/Redux/ChatBotPopup/chatBotPopupAction.jsx
@@ -1,5 +1,9 @@
 export const SetChatBotTableOrder = (state, action) => {
-  state?.chatbotData?.push(action?.payload);
+  state?.chatbotData?.push({...action?.payload});
+  let statusData = JSON.parse(localStorage.getItem("orderStatus")) || [];
+    let updatedData = [...statusData, action?.payload];
+    localStorage.setItem("orderStatus", JSON.stringify(updatedData));
+
 };
 
 export const GetChatBotTableOrder = (state, action) => {
